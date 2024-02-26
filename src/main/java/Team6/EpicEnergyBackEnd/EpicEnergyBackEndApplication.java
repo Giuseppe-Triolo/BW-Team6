248c070d8a0a825d90da606938e669f38bf236d4
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-02-26 12:37:13 +0100

    First commit

---
+++ src/main/java/Team6/EpicEnergyBackEnd/EpicEnergyBackEndApplication.java
@@ -0,0 +1,13 @@
+package Team6.EpicEnergyBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Team6/EpicEnergyBackEnd/EpicEnergyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Team6.EpicEnergyBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
