d17294e57f55a75f5422609e93d1b2e2d491e2b0
Katiacrisci <katcrisci@gmail.com>
2024-02-27 18:41:05 +0100

    fix InvoiceRepository

--- src/main/java/Team6/EpicEnergyBackEnd/repository/InvoiceRepository.java
+++ src/main/java/Team6/EpicEnergyBackEnd/repository/InvoiceRepository.java
@@ -2,10 +2,12 @@ package Team6.EpicEnergyBackEnd.repository;
 
 import Team6.EpicEnergyBackEnd.models.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     List<Invoice> findByClientId(String clientId);
